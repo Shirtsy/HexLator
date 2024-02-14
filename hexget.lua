0ae173c2a9e7443805eefae918070dd218221313
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2024-02-13 22:00:53 -0500

    Initialized

---
+++ hexget.lua
@@ -0,0 +1,85 @@
+local function printUsage()
+    local programName = arg[0] or fs.getName(shell.getRunningProgram())
+    print("Usage:")
+    print(programName .. " <url>")
+    print(programName .. " run <url>")
+end
+
+local version = "HexGet v1.0"
+print(version)
+print("-------------")
+sleep(0.5)
+
+local tArgs = { ... }
+
+local run = false
+if tArgs[1] == "run" then
+    table.remove(tArgs, 1)
+    run = true
+end
+
+if #tArgs < 1 then
+    printUsage()
+    return
+end
+
+local url = table.remove(tArgs, 1)
+
+if not http then
+    printError("hexget requires the http API")
+    printError("Set http.enabled to true in CC: Tweaked's config")
+    return
+end
+
+local function getFilename(sUrl)
+    sUrl = sUrl:gsub("[#?].*" , ""):gsub("/+$" , "")
+    return sUrl:match("/([^/]+)$")
+end
+
+local function get(sUrl)
+    -- Check if the URL is valid
+    local ok, err = http.checkURL(url)
+    if not ok then
+        printError(err or "Invalid URL.")
+        return
+    end
+
+    write("Connecting to " .. sUrl .. "... ")
+
+    local response = http.get(sUrl , nil , true)
+    if not response then
+        print("Failed.")
+        return nil
+    end
+
+    print("Success.")
+
+    local sResponse = response.readAll()
+    response.close()
+    return sResponse or ""
+end
+
+if run then
+    local res = get(url)
+    if not res then return end
+
+    local func, err = load(res, getFilename(url), "t", _ENV)
+    if not func then
+        printError(err)
+        return
+    end
+
+    local ok, err = pcall(func, table.unpack(tArgs))
+    if not ok then
+        printError(err)
+    end
+else
+    local res = get(url)
+    if not res then return end
+
+    local hexpiler = require("hexpiler")
+    local compiled = hexpiler.compile(res)
+    hexpiler.writeToFocus(compiled)
+end
+
+return {version = version}
---
+++ hexpiler.lua
@@ -0,0 +1,264 @@
+local function getRunningPath()
+    local runningProgram = shell.getRunningProgram()
+    local programName = fs.getName(runningProgram)
+    return runningProgram:sub( 1, #runningProgram - #programName )
+end
+
+--load symbol-registry.json
+local srFile = fs.open(getRunningPath() .. "symbol-registry.json", "r")
+local srRaw = textutils.unserialiseJSON(srFile.readAll())
+if not srFile then
+    print("Could not find symbol-registry.json in the current directory")
+    return
+end
+
+-- Strips all non-alphanumerics plus underscores
+local function stripString(iString)
+    local rString,_ = string.gsub(iString," ","_")
+    rString,_ = string.gsub(rString,"-","n")
+    rString,_ = string.gsub(rString,"+","p")
+    rString,_ = string.gsub(rString,"[^%w_]+","")
+    return rString
+end
+
+--load raw symbol registry translation table
+local symbolRegistry = {}
+for k,v in pairs(srRaw) do
+    local sName =  k --stripString(k)
+    symbolRegistry[sName] = {
+        ["angles"] = v["pattern"],
+        ["startDir"] = v["direction"],
+    }
+end
+symbolRegistry["{"] = symbolRegistry["Introspection"]
+symbolRegistry["}"] = symbolRegistry["Retrospection"]
+symbolRegistry[">>"] = symbolRegistry["Flock's Disintegration"]
+
+-- Given a string and start location, returns everything within a balanced set of parentheses
+local function getBalancedParens(s, startLoc)
+    local str = string.match(s, "(%b())", startLoc)
+    return string.sub(str, 2, -2)
+end
+
+-- Given a string, returns a table of strings with commas as delim
+local function splitCommas(str)
+    local valTable = {}
+    local i = 1
+    for k,_ in string.gmatch(str, "([^,]+)") do
+        valTable[i] = k
+        i = i + 1
+    end
+    return valTable
+end
+
+-- All identifiers and associated functions to correctly grab and format data given the raw string and a token
+local identRegistry = {
+    ["@null"] = function()
+        return {["null"] = true}
+    end,
+    ["@garbage"] = function()
+        return {["garbage"] = true}
+    end,
+    ["@true"] = function()
+        return true
+    end,
+    ["@false"] = function()
+        return false
+    end,
+    ["@iota_type"] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        return {["iotaType"] = str}
+    end,
+    ["@entity_type"] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        return {["entityType"] = str}
+    end,
+    ["@entity"] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        return {["uuid"] = str}
+    end,
+    ["@pattern"] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        local valTable = splitCommas(str)
+        local returnTable =  {
+            ["startDir"] = tonumber(valTable[1]),
+            ["angles"] = tonumber(valTable[2]),
+        }
+        return returnTable
+    end,
+    ["@gate"] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        return {["gate"] = str}
+    end,
+    ["@vec"] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        local valTable = splitCommas(str)
+        local returnTable =  {
+            ["x"] = tonumber(valTable[1]),
+            ["y"] = tonumber(valTable[2]),
+            ["z"] = tonumber(valTable[3])
+        }
+        return returnTable
+    end,
+    ["@num"] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        return tonumber(str)
+    end,
+    ['@str'] = function(s, token)
+        local str = getBalancedParens(s, token["start"])
+        return str
+    end,
+    ["%["] = true,
+    ["%]"] = true,
+}
+
+-- Runs a function associated with a token's 'content' field from a given reg table
+local function setIdentValue(s, registry, token)
+    local tokenReg = registry[token["content"]]
+    if tokenReg ~= nil and tokenReg ~= true then
+        token["value"] = tokenReg(s, token)
+    end
+end
+
+-- Gets symbol data associated with a token's 'content' field from a given reg table
+local function setSymbolValue(s, registry, token)
+    token["value"] = {
+        ["startDir"] = registry[token["content"]]["startDir"],
+        ["angles"] = registry[token["content"]]["angles"]
+    }
+end
+
+-- Walks through a string and checks for presence of tokens from registry and initializes them
+local function tokenSearch(s, registry)
+    local tokens = {}
+    for k,_ in pairs(registry) do
+        local i, j = string.find(s, k)
+        while i do
+            if not tokens[i] or j > tokens[i]["end"] then
+                tokens[i] = {
+                    ["start"] = i,
+                    ["end"] = j,
+                    ["content"] = k,
+                    ["value"] = nil
+                }
+            end
+            i, j = string.find(s, k, i+1)
+        end
+    end
+    return tokens
+end
+
+-- Sorts a list a tokens to occur in the order of their appearance in the raw string
+local function sortTokens(t)
+    local keys = {}
+    local returnTable = {}
+    for k,_ in pairs(t) do
+        table.insert(keys, k)
+    end
+    table.sort(keys)
+    for i, k in ipairs(keys) do
+        returnTable[i] = t[k]
+    end
+    return returnTable
+end
+
+-- Accepts a table of tables, returns a table that contains all key/value pairs
+local function combineTables(tTable)
+    local final = {}
+    for _,v in pairs(tTable) do
+        for k2,v2 in pairs(v) do
+            final[k2] = v2
+        end
+    end
+    return final
+end
+
+local myStack = {{}}
+local stack = {
+    push = function(e)
+        table.insert(myStack, e)
+    end,
+    pop = function()
+        return table.remove(myStack)
+    end,
+    top = function()
+        return myStack[#myStack]
+    end
+}
+
+local function dump_table(t,indent)
+    indent = indent or 0    
+    for key, value in pairs(t) do
+        if type(value) == "table" then
+            print(string.rep("  ", indent) .. key .. " = {")
+            dump_table(value, indent + 1)
+            print(string.rep("  ", indent) .. "}")
+        else
+            print(string.rep("  ", indent) .. key .. " = " .. tostring(value))
+        end
+    end
+end
+
+local function compileChunk(tokens)
+    local output = {}
+    for k,v in pairs(tokens) do
+        if v["content"] == "%[" then
+            print("Pushed...")
+            stack.push({})
+        elseif v["content"] == "%]" then
+            print("Popped...")
+            local j = stack.pop()
+            local t = stack.top()
+            table.insert(t, j)
+        else
+            local t = stack.top()
+            table.insert(t, v["value"])
+            --print(k,v["start"],v["end"]," ",v["content"], v["value"])
+        end
+    end
+    dump_table(myStack,0)
+    --dump_table(output,1)
+    return stack.top()
+end
+
+local function compile(str)
+    print("Compiling...")
+    local searches = {
+        ["symbols"] = tokenSearch(str, symbolRegistry),
+        ["identifiers"] = tokenSearch(str, identRegistry)
+    }
+    for _,v in pairs(searches["identifiers"]) do
+        setIdentValue(str, identRegistry, v)
+    end
+    for _,v in pairs(searches["symbols"]) do
+        setSymbolValue(str, symbolRegistry, v)
+    end
+    local tokens = sortTokens(combineTables(searches))
+    local output = compileChunk(tokens)
+    --print(#output)
+    return output
+end
+
+local function writeToFocus(tab)
+    --dump_table(tab,1)
+    local focal_port = peripheral.find("focal_port")
+    if not focal_port then
+        print("Cannot write! No focal port found.")
+    elseif not focal_port.hasFocus() then
+        print("Cannot write! No focus found.")
+    elseif not focal_port.canWriteIota() then
+        print("Cannot write! This won't compile!")
+    else
+        focal_port.writeIota(tab)
+        print("Compiled to focus!")
+    end
+end
+
+local function test()
+    local file = fs.open(getRunningPath() .. "example.hexpattern", "r")
+    local contents = file.readAll()
+    file.close()
+    writeToFocus(compile(contents))
+end
+
+return {compile = compile, writeToFocus = writeToFocus}
